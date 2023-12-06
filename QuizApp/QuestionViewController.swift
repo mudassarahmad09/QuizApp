a2a62bbe5ea85f3cd381fcc6d1602c6cbdb14be8
Qazi Mudassar Tanveer Ahmad <34540384+mudassarahmad09@users.noreply.github.com>
2023-12-06 20:34:05 +0500

    Start question view controller with some initial test with TDD

---
+++ QuizApp/QuestionViewController.swift
@@ -0,0 +1,37 @@
+//
+//  QuestionViewController.swift
+//  QuizApp
+//
+//  Created by Qazi Mudassar on 06/12/2023.
+//
+
+import UIKit
+
+class QuestionViewController: UIViewController {
+    @IBOutlet weak var headerLabel: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    private var question: String = ""
+    private var options: [String] = []
+    
+    convenience init(question: String,options: [String]) {
+        self.init()
+        self.question = question
+        self.options = options
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        headerLabel.text = question
+    }
+}
+extension QuestionViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        options.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = options[indexPath.row]
+        return cell
+    }
+}
