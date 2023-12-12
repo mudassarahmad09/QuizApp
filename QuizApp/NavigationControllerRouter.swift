ede4d03a89d3b2f9d2cb9e149c616e83b2ec88de
Qazi Mudassar Tanveer Ahmad <34540384+mudassarahmad09@users.noreply.github.com>
2023-12-12 13:20:26 +0500

    Start working on navigation contoller Router with TDD

---
+++ QuizApp/NavigationControllerRouter.swift
@@ -0,0 +1,12 @@
+//
+//  NavigationContorllerRouter.swift
+//  QuizApp
+//
+//  Created by Qazi Mudassar on 12/12/2023.
+//
+
+import UIKit
+
+class NavigationContorllerRouter {
+    
+}
---
+++ QuizAppTests/NavigationContorllerRouterTest.swift
@@ -0,0 +1,8 @@
+//
+//  NavigationContorllerRouterTest.swift
+//  QuizAppTests
+//
+//  Created by Qazi Mudassar on 12/12/2023.
+//
+
+import Foundation
