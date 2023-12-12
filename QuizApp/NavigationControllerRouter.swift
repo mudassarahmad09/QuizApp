d9f7685ff12561c00a49814b6e2b8a4418bba4cf
Qazi Mudassar Tanveer Ahmad <34540384+mudassarahmad09@users.noreply.github.com>
2023-12-12 16:04:28 +0500

    Adding the new type of question using the TDD and check for the equtable and hashable

--- QuizApp/NavigationControllerRouter.swift
+++ QuizApp/NavigationControllerRouter.swift
@@ -1,12 +1,33 @@
 //
-//  NavigationContorllerRouter.swift
+//  NavigationControllerRouter.swift
 //  QuizApp
 //
 //  Created by Qazi Mudassar on 12/12/2023.
 //
 
 import UIKit
+import QuizEngine
 
-class NavigationContorllerRouter {
+protocol ViewCotrollerFactory {
+    func questionViewController(for question: String, answerCallbacK: @escaping (String) -> (Void)) -> UIViewController
+}
+
+class NavigationControllerRouter: Router {
     
+    private let navigationController: UINavigationController
+    private let factory: ViewCotrollerFactory
+    
+    init(_ navigationController: UINavigationController, factory: ViewCotrollerFactory) {
+        self.navigationController = navigationController
+        self.factory = factory
+    }
+    
+    func routeTo(question: String, answerCallback: @escaping (String) -> Void) {
+        let viewController = factory.questionViewController(for: question, answerCallbacK: answerCallback)
+        navigationController.pushViewController(viewController, animated: true)
+    }
+    
+    func routeTo(result: Result<String, String>) {
+    
+    }
 }
--- QuizAppTests/NavigationContorllerRouterTest.swift
+++ QuizAppTests/NavigationContorllerRouterTest.swift
@@ -5,4 +5,57 @@
 //  Created by Qazi Mudassar on 12/12/2023.
 //
 
-import Foundation
+import XCTest
+import QuizEngine
+@testable import QuizApp
+
+class NavigationContorllerRouterTest: XCTestCase {
+    
+    let navigationController = NoneAnimatedNavigationContorller()
+    let factory = ViewControllerFactoryStub()
+    lazy var sut = {
+        NavigationControllerRouter(navigationController, factory: factory)
+    }()
+    
+    func test_routerToQuestions_showsQuestionController() {
+
+        let viewController =  UIViewController()
+        let secondviewController =  UIViewController()
+        factory.stub(question: "Q1", with: viewController)
+        factory.stub(question: "Q2", with: secondviewController)
+        
+        sut.routeTo(question: "Q1", answerCallback: {_ in })
+        sut.routeTo(question: "Q2", answerCallback: {_ in })
+        XCTAssertEqual(navigationController.viewControllers.count, 2)
+        XCTAssertEqual(navigationController.viewControllers.first, viewController)
+        XCTAssertEqual(navigationController.viewControllers.last, secondviewController)
+    }
+    
+    func test_routerToQuestion_presentQuestionControllerWithRightCallback() {
+        var callbackWasFired = false
+        sut.routeTo(question: "Q1", answerCallback: {_ in  callbackWasFired = true})
+        factory.answerCallback["Q1"]!("anything")
+        XCTAssertTrue(callbackWasFired)
+    }
+    
+    class NoneAnimatedNavigationContorller: UINavigationController {
+        override func pushViewController(_ viewController: UIViewController, animated: Bool) {
+            super.pushViewController(viewController, animated: false)
+        }
+    }
+    
+    class ViewControllerFactoryStub:  ViewCotrollerFactory {
+        
+        private var stubQuestions = [String: UIViewController]()
+        var answerCallback = [String: (String) -> (Void)]()
+        
+        func stub(question: String, with viewController: UIViewController) {
+            stubQuestions[question] = viewController
+        }
+        
+        func questionViewController(for question: String, answerCallbacK: @escaping (String) -> (Void)) -> UIViewController {
+            self.answerCallback[question] = answerCallbacK
+            return stubQuestions[question] ?? UIViewController()
+        }
+    }
+}
