fa399cda7fd88d8f36e01a60cf727b349fee26e2
Qazi Mudassar Tanveer Ahmad <34540384+mudassarahmad09@users.noreply.github.com>
2023-12-13 19:44:26 +0500

    check presenter logic for summay varable

--- QuizApp/ResultsPresenter.swift
+++ QuizApp/ResultsPresenter.swift
@@ -5,4 +5,17 @@
 //  Created by Qazi Mudassar on 13/12/2023.
 //
 
-import Foundation
+import QuizEngine
+
+struct ResultsPresenter {
+    private let resulte: Resulte<Question<String>, [String]>
+    
+    var summary: String {
+        "You got \(resulte.score)/\(resulte.answer.count) correct"
+    }
+    
+    init(resulte: Resulte<Question<String>, [String]>) {
+        self.resulte = resulte
+    }
+    
+}
--- QuizAppTests/ResultsPresenterTest.swift
+++ QuizAppTests/ResultsPresenterTest.swift
@@ -5,4 +5,25 @@
 //  Created by Qazi Mudassar on 13/12/2023.
 //
 
-import Foundation
+import XCTest
+import QuizEngine
+@testable import QuizApp
+class ResultsPresenterTest: XCTestCase {
+    
+    func test_summay_withTwoQuestionAndScoreOne_returnSummary() {
+        let answer = [Question.single("Q1"): ["A1"],
+                      Question.multiple("Q2"): ["A2", "A3"]]
+        let resulte = Resulte(answer: answer, score: 1)
+        let sut = ResultsPresenter(resulte: resulte)
+        XCTAssertEqual(sut.summary, "You got 1/2 correct")
+    }
+    
+    func test_summay_withThreeQuestionAndScoreTwo_returnSummary() {
+        let answer = [Question.single("Q1"): ["A1"],
+                      Question.multiple("Q2"): ["A2", "A3"],
+                      Question.multiple("Q3"): ["A3"]]
+        let resulte = Resulte(answer: answer, score: 2)
+        let sut = ResultsPresenter(resulte: resulte)
+        XCTAssertEqual(sut.summary, "You got 2/3 correct")
+    }
+}
