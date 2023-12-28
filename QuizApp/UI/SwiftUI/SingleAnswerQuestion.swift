5ca9124bacfff7f95239df467a272d987b5d1463
Qazi Mudassar Tanveer Ahmad <34540384+mudassarahmad09@users.noreply.github.com>
2023-12-28 18:02:41 +0500

    start adding the swiftui view

---
+++ QuizApp/UI/SwiftUI/SingleAnswerQuestion.swift
@@ -0,0 +1,18 @@
+//
+//  SingleAnswerQuestion.swift
+//  QuizApp
+//
+//  Created by Qazi Mudassar on 28/12/2023.
+//
+
+import SwiftUI
+
+struct SingleAnswerQuestion: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SingleAnswerQuestion()
+}
