0b4a8f32f4a3947c33c887464f082809990acd23
Qazi Mudassar Tanveer Ahmad <34540384+mudassarahmad09@users.noreply.github.com>
2023-12-28 18:19:14 +0500

    QuestionHeader is now seperat to resue in multiple spaces

---
+++ QuizApp/UI/SwiftUI/QuestionHeader.swift
@@ -0,0 +1,18 @@
+//
+//  QuestionHeader'.swift
+//  QuizApp
+//
+//  Created by Qazi Mudassar on 28/12/2023.
+//
+
+import SwiftUI
+
+struct QuestionHeader_: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    QuestionHeader_()
+}
