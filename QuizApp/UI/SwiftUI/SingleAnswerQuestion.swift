fcc56845885f4e5d737a9ade9f7de2e1d1a0b9c2
Qazi Mudassar Tanveer Ahmad <34540384+mudassarahmad09@users.noreply.github.com>
2023-12-28 18:03:08 +0500

    Add left file into the project as well

--- QuizApp/UI/SwiftUI/SingleAnswerQuestion.swift
+++ QuizApp/UI/SwiftUI/SingleAnswerQuestion.swift
@@ -8,11 +8,56 @@
 import SwiftUI
 
 struct SingleAnswerQuestion: View {
+    let title: String
+    let question: String
+    let options: [String]
+    let selection: (String) -> Void
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading, spacing: 0) {
+            
+            VStack(alignment: .leading, spacing: 15) {
+                Text(title)
+                    .font(.headline)
+                    .fontWeight(.medium)
+                    .foregroundColor(Color.blue)
+                    .padding(.top)
+                
+                Text(question)
+                    .font(.largeTitle)
+                    .fontWeight(.medium)
+            }.padding()
+            
+            ForEach(options, id: \.self) { option in
+                Button(action: {}, label: {
+                    HStack {
+                        Circle()
+                            .stroke(Color.secondary, lineWidth: 2.5)
+                            .frame(width: 30.0, height: 30.0)
+                        
+                        Text(option)
+                            .font(.title)
+                            .foregroundStyle(Color.secondary)
+                        
+                        Spacer()
+                    }.padding()
+                })
+            }
+            
+            Spacer()
+        }
     }
 }
 
 #Preview {
-    SingleAnswerQuestion()
+    SingleAnswerQuestion(
+        title: "1 of 2",
+        question: "What is Qazi Nationality?",
+        options: [
+            "Pakistan",
+            "India",
+            "USA",
+            "Canadian"
+        ],
+        selection: {_ in})
 }
