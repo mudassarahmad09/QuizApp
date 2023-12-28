d4f5e70fb66670b74754270dd3d5586f6aaf419d
Qazi Mudassar Tanveer Ahmad <34540384+mudassarahmad09@users.noreply.github.com>
2023-12-28 18:19:41 +0500

    QuestionHeader is seperate view now

--- QuizApp/UI/SwiftUI/QuestionHeader.swift
+++ QuizApp/UI/SwiftUI/QuestionHeader.swift
@@ -1,5 +1,5 @@
 //
-//  QuestionHeader'.swift
+//  QuestionHeader.swift
 //  QuizApp
 //
 //  Created by Qazi Mudassar on 28/12/2023.
@@ -7,12 +7,25 @@
 
 import SwiftUI
 
-struct QuestionHeader_: View {
+struct QuestionHeader: View {
+    let title: String
+    let question: String
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading, spacing: 15) {
+            Text(title)
+                .font(.headline)
+                .fontWeight(.medium)
+                .foregroundColor(Color.blue)
+                .padding(.top)
+            
+            Text(question)
+                .font(.largeTitle)
+                .fontWeight(.medium)
+        }.padding()
     }
 }
-
 #Preview {
-    QuestionHeader_()
+    QuestionHeader(title: "A title", question: "A question")
+        .previewLayout(.sizeThatFits)
 }
